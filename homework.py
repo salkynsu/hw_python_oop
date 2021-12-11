2b02062694f546a199a997bec1ef87b263c1b26d
salkynsu <salkyn.su@gmail.com>
2021-12-11 17:59:07 +0300

    6th commit on first review: pep8

--- homework.py
+++ homework.py
@@ -124,15 +124,16 @@ class SportsWalking(Training):
         """Получить количество затраченных калорий."""
         spent_calories = (
             (self.coeff_calorie_1
-            * self.weight
-            + (self.get_mean_speed() ** 2
-                // self.height
-             )
-            * self.coeff_calorie_2
-            * self.weight
-         )
-         * self.duration
-         * self.MIN_IN_H
+             * self.weight
+             + (self.get_mean_speed() 
+               ** 2
+               // self.height
+               )
+             * self.coeff_calorie_2
+             * self.weight
+            )
+            * self.duration
+            * self.MIN_IN_H
         )
         return spent_calories
 
